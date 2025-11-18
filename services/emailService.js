4ded5a00b6d987f409a8bf09a116126916a94a81
Manjeetmathur <manjeetkumar62054@gmail.com>
2025-11-18 22:18:10 +0530

    s

--- services/emailService.js
+++ services/emailService.js
@@ -10,8 +10,8 @@ const createTransporter = () => {
     port: parseInt(process.env.SMTP_PORT || '587'),
     secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
+      user: 'manjeetkumar62054@gmail.com',
+      pass: 'fazu jmlj iyqa mrki'
     }
   })
 }
